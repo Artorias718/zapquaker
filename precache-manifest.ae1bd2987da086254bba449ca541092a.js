1a70971c642c8d5ba7126e32f758ca52a1077134
Artorias718 <lai.ndr94@gmail.com>
2024-04-10 18:55:00 +0200

    Updates

---
+++ precache-manifest.ae1bd2987da086254bba449ca541092a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6079b33efb9316dd4361fa2e795ab382",
+    "url": "/zapquaker/index.html"
+  },
+  {
+    "revision": "718448cbef3165afac58",
+    "url": "/zapquaker/static/css/main.a13e9516.chunk.css"
+  },
+  {
+    "revision": "b9935ce0440f52076952",
+    "url": "/zapquaker/static/js/2.a33392e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zapquaker/static/js/2.a33392e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718448cbef3165afac58",
+    "url": "/zapquaker/static/js/main.2c9bd7b6.chunk.js"
+  },
+  {
+    "revision": "3a50d069fa33e46e0db7",
+    "url": "/zapquaker/static/js/runtime-main.25d69c2c.js"
+  }
+]);
